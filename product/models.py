6e0ff9575e1a58abe05bc0529495442bbcc6c359
pumalcn <pumalcn03@gmail.com>
2024-02-12 20:02:49 +0600

    Added management command to insert initial data

---
+++ product/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    featured = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['title']
+        verbose_name_plural = "Categories"
+
+    def __str__(self) -> str:  # Аннотация типа str
+        return self.title
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(unique=True)
+    featured = models.BooleanField(default=False)
+    thumbnail = models.URLField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+
